77cc22ea0ba4bc32216f9797b3b382bc1ea31d62
Evgeny Vlasenko <mahnunchik@gmail.com>
2025-10-22 13:05:23 +0100

    feat: jsdoc

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,12 @@
+import { jsdoc } from 'eslint-plugin-jsdoc';
 import config, { browser } from 'eslint-config-coinspace';
+
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   ...config,
   ...browser,
+  jsdoc({
+    config: 'flat/recommended',
+  }),
 ];
